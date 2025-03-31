b1b73e8a525f6289e69894aff57056ac3ad7ecf3
AriJusid <48042387@est.ort.edu.ar>
2025-03-31 15:39:15 -0300

    Hecho hasta el 4

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 var tareaIngresada = document.getElementById("ingreso")
 let listaTareas = []
-var checklist = document.getElementById("checklist")
-const agregarTarea = document.getElementById('botonIngresar')
 let lista = document.getElementById('list')
 
 
 function AgregarTarea(){
     const crearLabel = document.createElement ('li')
     const checkbox = document.createElement('input')
+    const crearDelete = document.createElement ('button')
+    const span = document.createElement('span')
 
+    crearDelete.innerHTML = "Eliminar"
     checkbox.setAttribute("type", "checkbox")
     
     listaTareas.push(tareaIngresada.value)
     listaTareas.forEach(function(tarea) {
-        crearLabel.innerHTML = tarea
+        span.innerHTML = tarea
     });
 
-    crearLabel.appendChild(checkbox)
-    lista.appendChild(crearLabel)
+    crearDelete.addEventListener("click", () => {
+        lista.removeChild(crearLabel)
+    })
 
+    crearLabel.appendChild(checkbox)
+    crearLabel.appendChild(span)
+    crearLabel.appendChild(crearDelete)
+    lista.appendChild(crearLabel)
 }
